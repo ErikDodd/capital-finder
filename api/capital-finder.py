6736b20a91984a8835a96e208de3c3e251c29641
Erik M. Dodd <emdodd27@gmail.com>
2023-01-09 15:06:20 -0800

    Even more refactoring

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -11,16 +11,16 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         dictionary = dict(query_string_list)
 
-        if "name" in dictionary:
+        if "country" in dictionary:
             url = "https://restcountries.com/v3.1/name/"
-            request = requests.get(url + dictionary["name"])
+            request = requests.get(url + dictionary["country"])
             data = request.json()
             countries_searched = ""
             for countries in data:
                 country = countries["capital"][0]
                 #countries_searched.append(country)
                 countries_searched += country
-            message = f"The capital of {dictionary['name']} is {countries_searched}"
+            message = f"The capital of {dictionary['country']} is {countries_searched}"
         else:
             message = "Please give us the name of a country"
 
