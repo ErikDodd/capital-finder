b083f751eb93e4b822d1fd32115d96e53c8e323d
Erik M. Dodd <emdodd27@gmail.com>
2023-01-09 14:18:35 -0800

    Added more code to capital-finder. Need to test it on vercel to see what happens

---
+++ api/capital-finder.py
@@ -0,0 +1,26 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        s = self.path
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+
+        url = "https://restcountries.com/v3.1/capital/"
+        user_input = input("> ")
+        request = requests.get(url + "?country=" + user_input)
+        data = request.json()
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+        message = "The capital of X is Y"
+        self.wfile.write(message.encode())
+
+        return
+
